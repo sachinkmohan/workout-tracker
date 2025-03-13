785a9dde5725e0be047027eaf444befa9f780ccf
Sachin KM <hiscan99@gmail.com>
2025-03-13 22:19:15 +0100

    Remove unused state management from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 
 import HomeScreen from "./components/HomeScreen";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div className="mt-10">
       <HomeScreen />
