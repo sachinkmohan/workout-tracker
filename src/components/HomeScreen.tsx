20ce12738b37a8f76920ed91e3ec7435b7298104
Sachin KM <hiscan99@gmail.com>
2025-03-13 22:13:34 +0100

    Add responsive Home Screen layout and add new icon for additional functionality

--- src/components/HomeScreen.tsx
+++ src/components/HomeScreen.tsx
@@ -1,7 +1,7 @@
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { useState } from "react";
-import { FaBeer } from "react-icons/fa";
+import { FaBeer, FaAccusoft } from "react-icons/fa";
 
 const HomeScreen = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(new Date());
@@ -13,30 +13,40 @@ const HomeScreen = () => {
   };
 
   return (
-    <div className="w-9/10 p-3 border border-indigo-400 rounded-xl mx-auto">
-      <div className="flex  flex-col justify-between ">
-        <div className="mb-3">
-          <select className="border border-gray-300 rounded-md px-3 py-1 focus:outline-none focus:ring-2 focus:ring-indigo-400">
+    <div className="w-9/10 md:max-w-[600px] p-3 border border-indigo-400 rounded-xl mx-auto">
+      <div className="flex flex-col">
+        <div className="mb-3 md:flex md:justify-items-start md:gap-4 ">
+          <select className="w-full md:w-1/3 border border-gray-300 rounded-md mb-3 px-3 py-1 focus:outline-none focus:ring-2 focus:ring-indigo-400">
             <option value="Strength-Total">Strength-Total</option>
             <option value="Strength-Upper">Strength-Upper</option>
             <option value="Strength-Lower">Strength-Lower</option>
             <option value="Yoga">Yoga</option>
             <option value="Abs">Abs</option>
           </select>
-          <button onClick={handleClickCalender}>
-            <FaBeer />
-          </button>
-          {isOpen && (
-            <DatePicker
-              selected={selectedDate}
-              onChange={(date) => setSelectedDate(date)}
-              dateFormat="dd/MM/yyyy h:mm aa"
-              showTimeInput
-              inline
-            />
-          )}
-        </div>
 
+          <div className="w-full md:w-1/3 mb-3 flex md:justify-items-start gap-2">
+            <button
+              onClick={handleClickCalender}
+              className="w-full md:w-auto flex justify-center border border-gray-300 rounded-md px-3 py-1 "
+            >
+              <FaBeer className="text-lg" />
+            </button>
+            {isOpen && (
+              <DatePicker
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
+                dateFormat="dd/MM/yyyy h:mm aa"
+                showTimeInput
+                inline
+              />
+            )}
+            <button className="w-full md:w-auto border border-gray-300 rounded-md px-3  py-1 flex justify-center">
+              <FaAccusoft className="text-lg" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div>
         <div className="flex gap-2 mb-3">
           {["5m", "10m", "20m", "30m", "45m"].map((value) => (
             <label key={value} className="flex gap-2">
@@ -46,7 +56,7 @@ const HomeScreen = () => {
           ))}
         </div>
 
-        <button className="bg-blue-300 text-white px-3 py-1 rounded-md">
+        <button className="w-full bg-blue-300 text-white px-3 py-1 rounded-md">
           Log
         </button>
       </div>
