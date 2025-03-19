e5910a207c80dda937d6538494c0d7185a66bc5a
Sachin KM <hiscan99@gmail.com>
2025-03-19 22:14:32 +0100

    Enhance LoggedWorkouts component with date formatting and improved layout

--- src/components/LoggedWorkouts.tsx
+++ src/components/LoggedWorkouts.tsx
@@ -1,3 +1,5 @@
+import { formatDistance, parseISO } from "date-fns";
+
 type WorkoutLog = {
   id: string;
   workoutName: string;
@@ -10,21 +12,27 @@ interface LoggedWorkoutsProps {
 }
 const LoggedWorkouts = ({ workoutLogs }: LoggedWorkoutsProps) => {
   return (
-    <div>
-      <h2>Logged Workouts</h2>
+    <div className="py-4">
+      <h2 className="mb-2 font-bold flex justify-items-start">
+        Logged Workouts
+      </h2>
       {workoutLogs?.length === 0 ? (
         <p>Flex that muscle & log a Workout </p>
       ) : (
         workoutLogs?.map((log) => (
           <div
             key={log.id}
-            className="p-2 flex justify-between border border-gray-200 rounded-lg "
+            className="p-2 mb-2 flex justify-between border border-gray-200 rounded-lg "
           >
             <div className="flex flex-col items-start">
               <p>{log.workoutName}</p>
               <p>{log.duration}</p>
             </div>
-            <p className="flex justify-center items-center">{log.logDate}</p>
+            <p className="flex justify-center items-center">
+              {formatDistance(parseISO(log.logDate), new Date(), {
+                addSuffix: true,
+              })}
+            </p>
           </div>
         ))
       )}
