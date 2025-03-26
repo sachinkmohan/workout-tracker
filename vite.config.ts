f78aa5630abb9175304fcbf789fdd0fdccf59281
Sachin KM <hiscan99@gmail.com>
2025-03-26 16:36:47 +0100

    Add PWA support with service worker and Vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,33 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        icons: [
+          {
+            src: "./src/assets/images/dumbbell-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "./src/assets/images/dumbbell-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+            purpose: "any",
+          },
+        ],
+      },
+    }),
+  ],
 });
